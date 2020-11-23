0fd3aa624fb9e891d405054b48fdda38cef4c53b
박진용 <jinyoung1997@gmail.com>
2020-11-23 18:20:14 +0900

    chore : project eslint 설정
    .vscode 에 client, server 별 인식 option 추가
    client eslint 추가

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,11 +1,11 @@
-import Koa, { Context } from "koa";
+import Koa, { Context } from 'koa';
 
 const app = new Koa();
 
 app.use((ctx: Context) => {
-  ctx.body = "hello world!";
+  ctx.body = 'hello world!';
 });
 
 app.listen(4000, () => {
-  console.log("Listening to port 4000");
+  console.log('Listening to port 4000');
 });
