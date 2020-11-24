5d44770ec785876e41b27480b534f49e1ac5ea6c
박진용 <jinyoung1997@gmail.com>
2020-11-24 20:36:42 +0900

    feat : kakao OAuth callback logic 만 구현
    isExistUser를 판단하고 없는 유저일시 생성

--- server/src/auth/auth.router.ts
+++ server/src/auth/auth.router.ts
@@ -1,5 +1,23 @@
+import { ACCESS_DENIED } from 'common/error';
 import Router from 'koa-router';
+import { Context } from 'koa';
+import * as AuthService from './auth.service';
+import KakaoUserInfo from './types/kakao-user-info';
+import UserService from '../user/user.service';
 
 const AuthRouter = new Router();
+
+AuthRouter.get('/callback', async (ctx: Context) => {
+  const { code, error } = ctx.request.query;
+  if (error || !code) throw new Error(ACCESS_DENIED);
+
+  const accessToken = await AuthService.getKakaoAccessToken(code);
+  const userData: KakaoUserInfo = await AuthService.getKakaoUserInfo(accessToken);
+  const isExistUser = await UserService.isExitSocialUser(userData.id, userData.type);
+  if (!isExistUser) {
+    const newUser = await UserService.createNewUser(userData);
+  }
+  const user = await UserService.getUserBySocialId(userData.id, userData.type);
+});
 
 export default AuthRouter;
