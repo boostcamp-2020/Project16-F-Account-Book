b7b31c03a4ff6556e458db0da96c6f6ed0edb87d
Changehee Choi <changhee.dev@gmail.com>
2020-11-25 03:27:02 +0900

    feat: seed 데이터 clear 모듈 구현
    현재 테이블의 데이터를 truncate 해주는 모듈을 구현함.

--- server/src/seed/clear.ts
+++ server/src/seed/clear.ts
@@ -1,11 +1,30 @@
-import { getManager } from "typeorm";
 import createDBConnection from '@/loader/database';
 import UserEntity from '@entity/user.entity';
+import CategoryEntity from '@entity/category.entity';
+import PaymentEntity from '@entity/payment.entity';
+import TransactionEntity from '@entity/transaction.entity';
 
 const clear = async () => {
-    await createDBConnection();
-    const entityManager = getManager();
-    await entityManager.clear(UserEntity);
-}
+  const connection = await createDBConnection();
+  const queryRunner = connection.createQueryRunner();
+  const { manager } = queryRunner;
+  await queryRunner.startTransaction();
+  try {
+    await manager.query('SET FOREIGN_KEY_CHECKS = 0');
+    await manager.clear(TransactionEntity);
+    await manager.clear(PaymentEntity);
+    await manager.clear(CategoryEntity);
+    await manager.clear(UserEntity);
+    await manager.query('SET FOREIGN_KEY_CHECKS = 1');
+    await queryRunner.commitTransaction();
+    console.log('Seed finished');
+  } catch (err) {
+    await queryRunner.rollbackTransaction();
+  } finally {
+    await queryRunner.release();
+    await connection.close();
+    console.log('Close connection');
+  }
+};
 
 clear();
