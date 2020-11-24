edbb0597d0dc68d72ac7bad823a00ff62f7da9e7
박진용 <jinyoung1997@gmail.com>
2020-11-24 21:12:46 +0900

    feat : kakao OAuth added
    인증 후 jwt token 발급 완료
    리팩토링 필요

--- server/src/auth/auth.router.ts
+++ server/src/auth/auth.router.ts
@@ -1,23 +1,36 @@
 import { ACCESS_DENIED } from 'common/error';
 import Router from 'koa-router';
 import { Context } from 'koa';
+import UserEntity from '@/entity/user.entity';
 import * as AuthService from './auth.service';
 import KakaoUserInfo from './types/kakao-user-info';
 import UserService from '../user/user.service';
 
 const AuthRouter = new Router();
 
-AuthRouter.get('/callback', async (ctx: Context) => {
+AuthRouter.get('/', (ctx: Context) => {
+  ctx.body = '리다이렉트됨';
+});
+
+AuthRouter.get('/callback/kakao', async (ctx: Context) => {
   const { code, error } = ctx.request.query;
   if (error || !code) throw new Error(ACCESS_DENIED);
 
   const accessToken = await AuthService.getKakaoAccessToken(code);
   const userData: KakaoUserInfo = await AuthService.getKakaoUserInfo(accessToken);
+  userData.type = 'kakao';
   const isExistUser = await UserService.isExitSocialUser(userData.id, userData.type);
-  if (!isExistUser) {
-    const newUser = await UserService.createNewUser(userData);
-  }
-  const user = await UserService.getUserBySocialId(userData.id, userData.type);
+
+  const user = isExistUser
+    ? ((await UserService.getUserBySocialId(userData.id, userData.type)) as UserEntity)
+    : ((await UserService.createNewUser(userData)) as UserEntity);
+
+  const jwtToken = AuthService.generateToken(user.uid);
+  ctx.cookies.set('jwt', jwtToken, {
+    maxAge: 1000 * 60 * 60 * 24 * 7,
+    httpOnly: true,
+  });
+  ctx.redirect('http://localhost:4000/api/auth');
 });
 
 export default AuthRouter;
