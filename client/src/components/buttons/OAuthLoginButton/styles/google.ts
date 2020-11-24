3a9f4a2590607d15feb5b272b1e5972f87c62b01
Changehee Choi <changhee.dev@gmail.com>
2020-11-24 16:27:31 +0900

    feat: OAuth 로그인 버튼 스타일 정의
    구글, 네이버, 카카오 OAuth 로그인 버튼의 스타일을 정의함.

---
+++ client/src/components/buttons/OAuthLoginButton/styles/google.ts
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+import defaultStyle from './default';
+
+const GoogleLoginButtonStyle = styled(defaultStyle)`
+  background: #fff;
+  background-image: url('/socials/google.png');
+  color: rgba(0, 0, 0, 0.54);
+  box-shadow: 0 1px 2px rgba(0, 0, 0, 0.27);
+`;
+
+export default GoogleLoginButtonStyle;
