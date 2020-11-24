76e373845c7229a30eb97d958a478e637d28a955
박진용 <jinyoung1997@gmail.com>
2020-11-24 17:29:11 +0900

    feat : kakao acessToken 및 userInfo 가져오기
    kaka oAuth 를 위해 필요한 method 구현

---
+++ server/src/auth/auth.service.ts
@@ -0,0 +1,34 @@
+import Axios from 'axios';
+import * as qs from 'querystring';
+
+const getKakaoAccessToken = async (code: string) => {
+  const response = await Axios.post(
+    'https://kauth.kakao.com/oauth/token',
+    qs.stringify({
+      grant_type: 'authorization_code',
+      code,
+      redirect_uri: process.env.REDIRECT_URI,
+      client_id: process.env.KAKAO_CLIENT_ID,
+      client_secret: process.env.KAKAO_CLIENT_SECRET,
+    }),
+    {
+      headers: {
+        'Content-type': 'application/x-www-form-urlencoded',
+        charset: 'utf-8',
+      },
+    },
+  );
+  return response.data.access_token;
+};
+
+const getKakaoUserInfo = async (accessToken: string) => {
+  const { data } = await Axios.get('https://kapi.kakao.com/v2/user/me', {
+    headers: {
+      Authorization: `Bearer ${accessToken}`,
+    },
+  });
+
+  return data;
+};
+
+export { getKakaoAccessToken, getKakaoUserInfo };
