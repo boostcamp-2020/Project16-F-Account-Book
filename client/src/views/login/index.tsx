0ccb757093ff2e0bbbdfa66eb71801d24fb0bf55
jeongsangwoo <jsw0413@gmail.com>
2020-11-26 00:53:02 +0900

    feat : 로그인 체크 기능 추가
    로그인 상태에서 로그인 페이지 접속시 대시보드 페이지로 이동 기능 구현

--- client/src/views/login/index.tsx
+++ client/src/views/login/index.tsx
@@ -1,9 +1,25 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { RouteComponentProps } from 'react-router-dom';
+import axios from 'axios';
 import Box from './style';
 import Logo from '../../components/Logo';
 import LoginButton from '../../container/loginButton';
 
-const LoginPage = (): JSX.Element => {
+const LoginPage = (props: RouteComponentProps): JSX.Element => {
+  useEffect(() => {
+    const checkLogin = async () => {
+      try {
+        const apiBaseUrl = process.env.REACT_APP_API_BASE_URL;
+        await axios.get(`${apiBaseUrl}/api/auth/isLogin`, {
+          withCredentials: true,
+        });
+        props.history.push('/dashboard');
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    checkLogin();
+  }, [props]);
   return (
     <Box>
       <Logo height="150px" />
