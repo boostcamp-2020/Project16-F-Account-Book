e8d61cd836b7ce047a720d3f2d995e09597e9a27
박진용 <jinyoung1997@gmail.com>
2020-11-26 01:49:43 +0900

    feat : App fullscreen 적용

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,16 +1,26 @@
 import React from 'react';
 import { Reset } from 'styled-reset';
 import { Redirect, Route, Switch } from 'react-router-dom';
+import styled from 'styled-components';
 import DashBoardPage from './views/dashBoard';
+
+const Box = styled.div`
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+`;
 
 function App(): JSX.Element {
   return (
     <>
       <Reset />
-      <Switch>
-        <Route path="/dashboard" component={DashBoardPage} />
-        <Redirect from="*" to="/dashboard" />
-      </Switch>
+      <Box>
+        <Switch>
+          <Route path="/dashboard" component={DashBoardPage} />
+          <Redirect from="*" to="/dashboard" />
+        </Switch>
+      </Box>
     </>
   );
 }
