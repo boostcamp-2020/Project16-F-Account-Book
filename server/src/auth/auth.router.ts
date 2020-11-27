c7da2f09cd4c23e707a41095fa7514c2a7a8b6b4
Changehee Choi <changhee.dev@gmail.com>
2020-11-27 14:45:40 +0900

    refactor: 인증 라우터 리팩토링
    기존의 분리된 콜백을 하나로 모으고 OAuthClient 모듈을 이용하는 방식으로
    리팩토링을 진행.

--- server/src/auth/auth.router.ts
+++ server/src/auth/auth.router.ts
@@ -1,78 +1,43 @@
 import { ACCESS_DENIED } from 'common/error';
 import Router from 'koa-router';
 import { Context } from 'koa';
-import AuthService from './auth.service';
-import KaKaoUserDTO from './types/kakao-user-dto';
+import { v4 } from 'uuid';
+import { JwtConfig } from '@config/index';
 import UserService from '../user/user.service';
-import NaverUserDTO from './types/naver-user-dto';
-import GoogleUserDTO from './types/google-user-dto';
 import jwtAuthorize from '../middleware/jwt-authorize';
 import OAuthClient from './oauth-client';
+import JwtUtils from './utils/jwt-utils';
 
 const AuthRouter = new Router();
 
 AuthRouter.get('/isLogin', jwtAuthorize, (ctx: Context) => {
   ctx.status = 200;
 });
+
 AuthRouter.get('/:provider', (ctx: Context) => {
   const { provider } = ctx.params;
   const oAuthClient = new OAuthClient(provider);
-  oAuthClient.redirectToAuthorizaionPage(ctx);
+  const state = v4();
+  oAuthClient.redirectToAuthorizaionPage(ctx, state);
 });
 
-AuthRouter.get('/callback/naver/redirect', async (ctx: Context) => {
-  const { code, error } = ctx.request.query;
+AuthRouter.get('/callback/:provider', async (ctx: Context) => {
+  const { provider } = ctx.params;
+  const { code, state, error } = ctx.request.query;
   if (error || !code) throw new Error(ACCESS_DENIED);
 
-  const accessToken = await AuthService.getNaverAccessToken(code);
-  const userInfo = await AuthService.getNaverUserInfo(accessToken);
-  const userData = new NaverUserDTO(userInfo);
+  const oAuthClient = new OAuthClient(provider);
+  const accessToken = await oAuthClient.getAccessToken(code, state);
+  const userInfo = await oAuthClient.getUserInfo(accessToken);
 
-  const uid = await UserService.getOrCreateUid(userData);
+  const uid = await UserService.getOrCreateUid(userInfo);
+  const jwtToken = JwtUtils.generateToken(uid);
 
-  const jwtToken = AuthService.generateToken(uid);
   ctx.cookies.set('jwt', jwtToken, {
-    maxAge: Number(process.env.TOKEN_EXP),
+    maxAge: JwtConfig.cookieExpiredAt,
     httpOnly: true,
   });
-  const clientUri = process.env.CLIENT_URI as string;
-  ctx.redirect(clientUri);
-});
 
-AuthRouter.get('/callback/kakao', async (ctx: Context) => {
-  const { code, error } = ctx.request.query;
-  if (error || !code) throw new Error(ACCESS_DENIED);
-
-  const accessToken = await AuthService.getKakaoAccessToken(code);
-  const userInfo = await AuthService.getKakaoUserInfo(accessToken);
-  const userData = new KaKaoUserDTO(userInfo);
-
-  const uid = await UserService.getOrCreateUid(userData);
-
-  const jwtToken = AuthService.generateToken(uid);
-  ctx.cookies.set('jwt', jwtToken, {
-    maxAge: Number(process.env.TOKEN_EXP),
-    httpOnly: true,
-  });
-  const clientUri = process.env.CLIENT_URI as string;
-  ctx.redirect(clientUri);
-});
-
-AuthRouter.get('/callback/google', async (ctx: Context) => {
-  const { code, error } = ctx.request.query;
-  if (error || !code) throw new Error(ACCESS_DENIED);
-
-  const accessToken = await AuthService.getGoogleAccessToken(code);
-  const userInfo = await AuthService.getGoogleUserInfo(accessToken);
-  const userData = new GoogleUserDTO(userInfo);
-
-  const uid = await UserService.getOrCreateUid(userData);
-  console.log(userInfo);
-  const jwtToken = AuthService.generateToken(uid);
-  ctx.cookies.set('jwt', jwtToken, {
-    maxAge: Number(process.env.TOKEN_EXP),
-    httpOnly: true,
-  });
   const clientUri = process.env.CLIENT_URI as string;
   ctx.redirect(clientUri);
 });
