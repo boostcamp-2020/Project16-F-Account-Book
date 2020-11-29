1a5169983d95f3669378112a95d7e6579505fc91
Changehee Choi <changhee.dev@gmail.com>
2020-11-29 20:54:21 +0900

    feat: TabMenu 컴포넌트 스타일 구현

---
+++ client/src/components/common/TabMenu/index.stories.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import TabMenu from '.';
+
+export default {
+  title: 'components/TabMenu',
+  component: TabMenu,
+};
+
+const list = [
+  {
+    tabName: 'Tab1',
+    children: <p>Tab1 내용입니다.</p>,
+  },
+  {
+    tabName: 'Tab2',
+    children: <p>Tab2 내용입니다.</p>,
+  },
+  {
+    tabName: 'Tab3',
+    children: <p>Tab3 내용입니다.</p>,
+  },
+];
+
+export const tab = (): JSX.Element => <TabMenu list={list} />;
---
+++ client/src/components/common/TabMenu/styles.ts
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+export const StyledTabMenuButton = styled.button`
+  flex: 1 auto;
+  border: none;
+  font-size: 1.2rem;
+  padding: 0.3rem 0.8rem;
+  background: transparent;
+`;
+
+export const StyledTabMenuButtonGroup = styled.div`
+  display: flex;
+  border-bottom: 1px solid rgba(0, 0, 0, 0.3);
+`;
