6857fa68f453a564aa73e8f9de79a4da669df13a
Changehee Choi <changhee.dev@gmail.com>
2020-11-30 18:06:38 +0900

    refactor: Logo 컴포넌트 수정
    - svg 태그 코드를 .svg 파일로 분리함.
    - 좌우 밸런스를 맞추기 위한 margin-right 속성 추가.
    - Logo 컴포넌트의 width를 이미지 크기만큼 생성되도록 inline-block 으로 변경.

--- client/src/components/common/Logo/style.ts
+++ client/src/components/common/Logo/style.ts
@@ -2,8 +2,10 @@ import styled from 'styled-components';
 import { LogoPropType } from './types';
 
 const StyledLogo = styled.div`
+  display: inline-block;
   height: ${(props: LogoPropType) => props.height};
-  svg {
+  margin-right: -2.5rem;
+  img {
     height: 100%;
   }
 `;
