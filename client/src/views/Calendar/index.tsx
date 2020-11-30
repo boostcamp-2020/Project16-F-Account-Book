cf7b58efe19ab0da3dd748dc9937a3b513ddb9a8
parkdit94 <dw1156@naver.com>
2020-11-30 22:58:51 +0900

    style:arrow.svg 색 black => #9e9e9e로 회색으로 변경

--- client/src/views/Calendar/index.tsx
+++ client/src/views/Calendar/index.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 
 import Header from '@/components/Header';
+import Arrow from '@/components/common/Arrow';
 
 const CalendarPage = (): JSX.Element => {
   return (
     <>
       <Header />
+      <Arrow />
     </>
   );
 };
