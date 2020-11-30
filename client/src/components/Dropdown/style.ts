3f3355235ea7a905c9044cb4ce269d52efd4ef89
parkdit94 <dw1156@naver.com>
2020-11-30 21:42:40 +0900

    style:header style 변경

--- client/src/components/Dropdown/style.ts
+++ client/src/components/Dropdown/style.ts
@@ -1,10 +1,10 @@
 import styled from 'styled-components';
 
 const name = styled.li`
-  margin: 20px 8px;
+  margin: 13px 8px;
   white-space: nowarp;
   font-weight: bold;
-  font-size: 20px;
+  font-size: 18px;
   word-break: keep-all;
   list-style: none;
 `;
