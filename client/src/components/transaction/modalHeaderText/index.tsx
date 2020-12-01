c0136fcc61899f2e57e97ceb0711c6977b67853e
namda-on <namda_on@DESKTOP-M63L4UT.localdomain>
2020-12-01 17:47:48 +0900

    refactor : pascal case 충돌 해결
    untracking 되는 소문자 directory 제거

--- client/src/components/transaction/modalHeaderText/index.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { ModalHeaderProps } from './types';
-import ModalHeaderTextStyle from './styles';
-
-function ModalHeaderText({ children }: ModalHeaderProps): JSX.Element {
-  return <ModalHeaderTextStyle>{children}</ModalHeaderTextStyle>;
-}
-
-export default ModalHeaderText;
--- client/src/components/transaction/modalXButton/index.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import XButton from './styles';
-import { ModalXButtonProps } from './types';
-
-function ModalXButton({ onClickEvent }: ModalXButtonProps): JSX.Element {
-  return <XButton onClick={onClickEvent}>X</XButton>;
-}
-
-export default ModalXButton;
