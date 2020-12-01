8dd8900dd65aeef7c6a3da2687fdf376dd4c31ea
jeongsangwoo <jsw0413@gmail.com>
2020-12-01 22:22:39 +0900

    feat : redux devtools 추가
    redux devtools 추가

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -3,12 +3,13 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 import { applyMiddleware, createStore } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import ReduxThunk from 'redux-thunk';
 import App from './App';
 import rootReducer from './modules';
 import reportWebVitals from './reportWebVitals';
 
-const store = createStore(rootReducer, applyMiddleware(ReduxThunk));
+const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(ReduxThunk)));
 
 ReactDOM.render(
   <React.StrictMode>
