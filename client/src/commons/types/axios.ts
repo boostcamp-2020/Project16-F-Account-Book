f544895362548b5c0fdd9e3e731ba6f189f9440d
Changehee Choi <changhee.dev@gmail.com>
2020-12-01 21:20:18 +0900

    feat: axios 응답 type 재정의
    axios interceptor를 이용해 response의 data를 반환하도록 설정된것에
    맞춰서 기존 응답 타입을 재정의함.

---
+++ client/src/commons/types/axios.ts
@@ -0,0 +1,14 @@
+// Code Reference: https://github.com/axios/axios/issues/1510
+import 'axios';
+
+declare module 'axios' {
+  export interface AxiosInstance {
+    request<T = any>(config: AxiosRequestConfig): Promise<T>;
+    get<T = any>(url: string, config?: AxiosRequestConfig): Promise<T>;
+    delete<T = any>(url: string, config?: AxiosRequestConfig): Promise<T>;
+    head<T = any>(url: string, config?: AxiosRequestConfig): Promise<T>;
+    post<T = any>(url: string, data?: any, config?: AxiosRequestConfig): Promise<T>;
+    put<T = any>(url: string, data?: any, config?: AxiosRequestConfig): Promise<T>;
+    patch<T = any>(url: string, data?: any, config?: AxiosRequestConfig): Promise<T>;
+  }
+}
