c13678881571e5e4964d19777d85600fa82cd3e4
박진용 <jinyoung1997@gmail.com>
2020-12-01 10:59:51 +0900

    feat : 결제수단 가져오기 구현

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Reset } from 'styled-reset';
 import { Redirect, Route, Switch } from 'react-router-dom';
 import styled from 'styled-components';
-import DashBoardPage from './views/dashBoard';
+import DashBoardPage from './views/dashBoard/index';
 import LoginPage from './views/login';
 
 const Box = styled.div`
