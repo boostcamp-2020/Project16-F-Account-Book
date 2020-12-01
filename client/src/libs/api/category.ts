11c187703e874e997613d97160e065fb21acc3e9
Changehee Choi <changhee.dev@gmail.com>
2020-12-01 22:00:26 +0900

    refactor: category, payment API 호출 모듈 리팩토링
    프로젝트에서 생성한 axios 인스턴스를 사용하도록 변경함.

--- client/src/libs/api/category.ts
+++ client/src/libs/api/category.ts
@@ -1,13 +1,11 @@
+import axios from '@/libs/axios';
+import endpoints from '@/libs/endpoints';
 import { CategoryModel } from '@commons/types/category';
-import Axios from 'axios';
 
 const categoryAPI = {
   getCategory: async (): Promise<CategoryModel[]> => {
-    const response = await Axios.get(`${process.env.REACT_APP_API_BASE_URL}/api/categories`, {
-      withCredentials: true,
-    });
-
-    return response.data;
+    const categories = await axios.get<CategoryModel[]>(endpoints.CATEGORY_API);
+    return categories;
   },
 };
 
