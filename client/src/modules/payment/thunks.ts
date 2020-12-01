6712249dddd42e15e8dd59644f7535382b6683d0
박진용 <jinyoung1997@gmail.com>
2020-12-01 11:38:34 +0900

    refactor : thunk 함수 리팩토링 >> >> createAsyncThunk 를 이용하여 refactoring

--- client/src/modules/payment/thunks.ts
+++ client/src/modules/payment/thunks.ts
@@ -1,26 +1,8 @@
-import { ThunkAction } from 'redux-thunk';
-import { $CombinedState, Dispatch } from 'redux';
 import paymentAPI from '@libs/api/payment';
-import { PaymentAction, PaymentState } from './types';
+import createAsyncThunk from '@libs/createAsyncThunk';
 import { getPaymentAsync } from './actions';
 
-type State = {
-  readonly [$CombinedState]?: undefined;
-} & { payment: PaymentState };
-
-const getPaymentThunk = (): ThunkAction<void, State, null, PaymentAction> => async (
-  dispatch: Dispatch,
-) => {
-  const { request, success, failure } = getPaymentAsync;
-  dispatch(request());
-  try {
-    const payments = await paymentAPI.getPayment();
-    dispatch(success(payments));
-  } catch (e) {
-    dispatch(failure(e));
-    throw e;
-  }
-};
+const getPaymentThunk = createAsyncThunk(getPaymentAsync, paymentAPI.getPayment);
 
 // eslint-disable-next-line import/prefer-default-export
 export { getPaymentThunk };
