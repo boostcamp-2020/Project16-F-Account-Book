f85ba5a5f06dbd49e73d9b96bc11c82e79dda1d6
parkdit94 <dw1156@naver.com>
2020-12-01 10:51:58 +0900

    style: ArrowStorybook width 오타 수정 20 => 20px

--- client/src/components/common/Arrow/index.stories.tsx
+++ client/src/components/common/Arrow/index.stories.tsx
@@ -6,6 +6,6 @@ export default {
   component: Arrow,
 };
 
-export const ArrowDownButton = (): JSX.Element => <Arrow height="10px" width="20" rotate="90" />;
+export const ArrowDownButton = (): JSX.Element => <Arrow height="10px" width="20px" rotate="90" />;
 
-export const ArrowUpButton = (): JSX.Element => <Arrow height="10px" width="20" rotate="180" />;
+export const ArrowUpButton = (): JSX.Element => <Arrow height="10px" width="20px" rotate="180" />;
