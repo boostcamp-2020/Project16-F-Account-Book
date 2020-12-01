2dae025936a0ee10d2b906f77ba9b4d2c0ac31d8
박진용 <jinyoung1997@gmail.com>
2020-12-01 22:36:11 +0900

    feat : postTransaction api 추가

--- client/src/libs/api/transaction.ts
+++ client/src/libs/api/transaction.ts
@@ -1,6 +1,6 @@
 import axios from '@/libs/axios';
 import endpoints from '@/libs/endpoints';
-import { MonthTransactionsResponse } from '@/commons/types/transaction';
+import { MonthTransactionsResponse, PostTransactionRequest } from '@/commons/types/transaction';
 
 const transactionAPI = {
   getMonthTransactions: async ({
@@ -13,7 +13,22 @@ const transactionAPI = {
     const monthlyTransactionDetails = await axios.get<MonthTransactionsResponse>(
       `${endpoints.TRANSACTION_API}?year=${year}&month=${month}`,
     );
+
     return monthlyTransactionDetails;
+  },
+
+  postTransaction: async (data: PostTransactionRequest) => {
+    const newTransaction = await axios.post(
+      `${endpoints.TRANSACTION_API}`,
+      { ...data },
+      {
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      },
+    );
+
+    return newTransaction;
   },
 };
 
