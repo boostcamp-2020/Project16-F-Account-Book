e1fe8ed0284b7d27b5be2cbe9e6ee5b52c921cc7
박진용 <jinyoung1997@gmail.com>
2020-12-01 11:36:31 +0900

    feat : createAsyncThunk 추가
    이후에 생성할 thunk함수를 간편하게 만들기 위하여 library 화

---
+++ client/src/libs/createAsyncThunk.ts
@@ -0,0 +1,23 @@
+import { Dispatch } from 'redux';
+import { AsyncActionCreatorBuilder } from 'typesafe-actions';
+
+type AnyAsyncActionCreator = AsyncActionCreatorBuilder<any, any, any>;
+
+export default function createAsyncThunk<
+  A extends AnyAsyncActionCreator,
+  F extends (...params: any[]) => Promise<any>
+>(asyncActionCreator: A, promiseCreator: F) {
+  type Params = Parameters<F>;
+  return function thunk(...params: Params) {
+    return async (dispatch: Dispatch) => {
+      const { request, success, failure } = asyncActionCreator;
+      dispatch(request(undefined)); // 파라미터를 비우면 타입 에러가 나기 때문에 undefined 전달
+      try {
+        const result = await promiseCreator(...params);
+        dispatch(success(result));
+      } catch (e) {
+        dispatch(failure(e));
+      }
+    };
+  };
+}
