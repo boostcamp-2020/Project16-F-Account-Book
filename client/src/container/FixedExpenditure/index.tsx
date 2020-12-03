3818f5215ed1d6624baac04c219685c1f527fcfd
Sangwoo <66261552+sangw3433@users.noreply.github.com>
2020-12-04 00:54:14 +0900

    feat : 고정 지출 컨테이너 구현
    고정 지출 컨테이너 구현

--- client/src/container/FixedExpenditure/index.tsx
+++ client/src/container/FixedExpenditure/index.tsx
@@ -1,11 +1,15 @@
-import React, { useCallback, useEffect } from 'react';
+/* eslint-disable no-restricted-syntax */
+import React, { useCallback, useMemo, useEffect } from 'react';
 import { getFixedExpenditureThunk } from '@modules/fixedExpenditure';
 import { useSelector, useDispatch } from 'react-redux';
 import { RootState } from '@/modules';
 import dateUtils from '@libs/dateUtils';
+import { Link } from 'react-router-dom';
+import { GoCalendar } from 'react-icons/go';
+import * as S from './styles';
 
 const FixedExpenditure = (): JSX.Element => {
-  const { datePicker } = useSelector((state: RootState) => state);
+  const { datePicker, fixedExpenditure } = useSelector((state: RootState) => state);
   const dispatch = useDispatch();
 
   const getFixedExpenditure = useCallback(() => {
@@ -13,10 +17,41 @@ const FixedExpenditure = (): JSX.Element => {
     dispatch(getFixedExpenditureThunk(startDate, endDate));
   }, [dispatch, datePicker]);
 
+  const getSumFixedExpenditure = useCallback(() => {
+    const { data } = fixedExpenditure.fixedExpenditure;
+    if (data) {
+      let sum = 0;
+      for (const value of data) {
+        sum += value.amount;
+      }
+      return sum;
+    }
+    return 0;
+  }, [fixedExpenditure]);
+
   useEffect(() => {
     getFixedExpenditure();
   }, [dispatch, datePicker]);
-  return <></>;
+  return (
+    <>
+      <S.Box>
+        <S.HeaderBox>
+          <S.HeaderTitle>
+            <p>예정된 고정지출이</p>
+            <p>{fixedExpenditure.fixedExpenditure.data?.length}개 있어요</p>
+          </S.HeaderTitle>
+          <Link to="/detail-fixed-expenditure">자세히 보기</Link>
+        </S.HeaderBox>
+        <S.SumBox>
+          <GoCalendar />
+          <S.TextBox>
+            <S.SumTitle>예정된 고정 지출</S.SumTitle>
+            <S.Amount>{getSumFixedExpenditure()}원</S.Amount>
+          </S.TextBox>
+        </S.SumBox>
+      </S.Box>
+    </>
+  );
 };
 
 export default FixedExpenditure;
