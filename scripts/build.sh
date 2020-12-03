44ca5a778f80293da3840d87232840c7571d7924
Changehee Choi <changhee.dev@gmail.com>
2020-12-04 04:52:29 +0900

    chore: Docker 이미지 생성 및 푸쉬 스크립트 작성
    docker-compose.build.yml 을 이용하여 Dockerfile을 빌드하고 생성된
    이미지를 DockerHub로 push 하는 로직의 스크립트 작성.
    
    스크립트의 인자로 배포버전, dockerhub 계정, dockerhub 패스워드를
    입력받고 배포 버전이 pre-release가 아니면 latest 버전을 추가로 배포한다.

---
+++ scripts/build.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+tags=($1)
+DOCKER_USER=$2
+DOCKER_PASSWORD=$3
+
+serverRepoName="${DOCKER_USER}/accountbook-server"
+clientRepoName="${DOCKER_USER}/accountbook-client"
+
+docker login --username $DOCKER_USER --password $DOCKER_PASSWORD || exit 1
+
+yarn install && yarn build
+
+docker-compose -f docker-compose.build.yml build --parallel
+
+if [ ${tags[0]} != "pre-release" ]
+then 
+    tags+=("latest")
+fi
+
+for tag in ${tags[@]}; do
+    docker tag accountbook-server:pre-release ${serverRepoName}:${tag}
+    docker tag accountbook-client:pre-release ${clientRepoName}:${tag}
+    docker push ${serverRepoName}:${tag}
+    docker push ${clientRepoName}:${tag}
+done
