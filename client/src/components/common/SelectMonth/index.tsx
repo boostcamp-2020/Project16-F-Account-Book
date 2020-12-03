ac2fa1e6be8e535c5d340af619b2ac3db544463b
parkdit94 <dw1156@naver.com>
2020-12-03 21:22:24 +0900

    feat: 타입추가

--- client/src/components/common/SelectMonth/index.tsx
+++ client/src/components/common/SelectMonth/index.tsx
@@ -2,11 +2,27 @@ import React, { useState } from 'react';
 
 import Dropdown from '@components/common/Dropdown';
 import ArrowIcon from '@/components/common/ArrowIcon';
+import { SelectMonthType } from './type';
+import * as S from './styles';
 
 const MONTHSLIST = Array.from({ length: 12 }, (v, i) => String(i + 1));
 
 export default function SelectMonth({ month }: SelectMonthType): JSX.Element {
+  const [Month, setMonth] = useState(month);
   const dropDonwList = MONTHSLIST.map((v: string) => <li>{v}</li>);
 
-  return <></>;
+  return (
+    <>
+      <S.MonthDiv>
+        {Month}월
+        <S.DropdownPosition>
+          <Dropdown icon={<ArrowIcon height="15px" width="20px" rotate="" />} isRight={false}>
+            <S.Item>
+              <S.ScollDiv>{dropDonwList}</S.ScollDiv>
+            </S.Item>
+          </Dropdown>
+        </S.DropdownPosition>
+      </S.MonthDiv>
+    </>
+  );
 }
