4aefc1d08ab86c7e19a058361f2c0c349514cfd8
parkdit94 <dw1156@naver.com>
2020-12-03 22:03:35 +0900

    매트릭스 뷰 컴포넌트 구현

---
+++ client/src/components/calendar/MatrixView/index.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { MatrixViewTypes } from './types';
+import * as S from './styles';
+
+function MatrixView({ matrix, headers, cell }: MatrixViewTypes): JSX.Element {
+  return (
+    <S.Matrix>
+      <S.Table>
+        <S.Thead>
+          <S.Tr>
+            <>
+              {headers.map((v) => (
+                <S.Th>{v}</S.Th>
+              ))}
+            </>
+          </S.Tr>
+        </S.Thead>
+        <S.Tbody>
+          <>
+            {matrix.map((row, i) => (
+              <S.Tr>{row.map((v, j) => cell(v, i * matrix[i].length + j))}</S.Tr>
+            ))}
+          </>
+        </S.Tbody>
+      </S.Table>
+    </S.Matrix>
+  );
+}
+
+export default MatrixView;
