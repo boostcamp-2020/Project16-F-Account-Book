b59224136ccf1cb8fd11cb6a6a29f1caadbe23c6
parkdit94 <dw1156@naver.com>
2020-12-03 21:23:37 +0900

    feat: 스타일 추가

---
+++ client/src/components/common/SelectMonth/styles.tsx
@@ -0,0 +1,36 @@
+import styled from 'styled-components';
+
+const MonthDiv = styled.div`
+  display: flex;
+`;
+
+const ScollDiv = styled.div`
+  overflow: auto;
+  height: 100px;
+  width: 30px;
+  &::-webkit-scrollbar-thumb {
+    background-color: grey;
+    border-radius: 10px;
+    background-clip: padding-box;
+    border: 2px solid transparent;
+  }
+  &::-webkit-scrollbar-track {
+    border-radius: 5px;
+    box-shadow: inset 0px 0px 5px white;
+  }
+`;
+
+const DropdownPosition = styled.div`
+  text-align: top;
+  margin-top: -5px;
+`;
+const Item = styled.li`
+  display: inline-block;
+  white-space: nowarp;
+  font-weight: bold;
+  font-size: 16px;
+  word-break: keep-all;
+  list-style: none;
+`;
+
+export { DropdownPosition, Item, ScollDiv, MonthDiv };
