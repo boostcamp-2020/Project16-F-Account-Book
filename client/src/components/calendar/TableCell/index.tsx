a8726eeff44214c419790d89e39a494f6df6607e
parkdit94 <dw1156@naver.com>
2020-12-03 21:56:55 +0900

    feat:테이블셀에서 가격에 따른 AmountText 크기 수정기능 추가

--- client/src/components/calendar/TableCell/index.tsx
+++ client/src/components/calendar/TableCell/index.tsx
@@ -3,6 +3,13 @@ import AmountText from '@/components/transaction/AmountText';
 import { TableCellTypes } from './types';
 import * as S from './style';
 
+const getRem = (n: number): string => {
+  let rem = '0.8rem';
+  const len: number = String(n).length;
+  if (len > 5 && len < 7) rem = '0.7rem';
+  if (len > 6) rem = '0.1rem';
+  return rem;
+};
 function TableCell({ day, totalInOut }: TableCellTypes): JSX.Element {
   return (
     <td>
@@ -10,7 +17,11 @@ function TableCell({ day, totalInOut }: TableCellTypes): JSX.Element {
       <S.TotalIn>
         {totalInOut[day] && totalInOut[day].totalIn > 0 ? (
           <div>
-            <AmountText isIncome amount={totalInOut[day].totalIn} />
+            <AmountText
+              isIncome
+              amount={totalInOut[day].totalIn}
+              size={getRem(totalInOut[day].totalIn)}
+            />
           </div>
         ) : (
           ''
@@ -19,7 +30,11 @@ function TableCell({ day, totalInOut }: TableCellTypes): JSX.Element {
       <S.TotalOut>
         {totalInOut[day] && totalInOut[day].totalOut > 0 ? (
           <div>
-            <AmountText isIncome={false} amount={totalInOut[day].totalOut} />
+            <AmountText
+              isIncome={false}
+              amount={totalInOut[day].totalOut}
+              size={getRem(totalInOut[day].totalOut)}
+            />
           </div>
         ) : (
           ''
