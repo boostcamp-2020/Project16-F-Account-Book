a57c6c79544efdc20ee8a026ab1e3bea441e967d
parkdit94 <dw1156@naver.com>
2020-12-03 22:14:53 +0900

    feat:캘린더뷰 스토리북 구현

--- client/src/components/calendar/CalendarView/indes.stories.tsx
+++ client/src/components/calendar/CalendarView/indes.stories.tsx
@@ -5,3 +5,20 @@ export default {
   title: 'components/calendar/CalendarView',
   component: CalendarView,
 };
+
+const totalInOut: any = {
+  1: { totalIn: 150, totalOut: 5000 },
+  5: { totalIn: 10000, totalOut: 0 },
+  13: { totalIn: 10000, totalOut: 0 },
+  18: { totalIn: 50000, totalOut: 40000 },
+  20: { totalIn: 20000, totalOut: 25000 },
+  21: { totalIn: 0, totalOut: 5000 },
+  22: { totalIn: 10000, totalOut: 0 },
+  23: { totalIn: 0, totalOut: 5000 },
+  27: { totalIn: 10000, totalOut: 0 },
+  30: { totalIn: 0, totalOut: 10000 },
+};
+
+export const ShowCalendar = (): JSX.Element => (
+  <CalendarView totalInOut={totalInOut} lang="ko" year={2020} month={12} />
+);
