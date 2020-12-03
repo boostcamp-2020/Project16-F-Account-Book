108ab53efecb445bb3be32fc73637cfaaa68281d
Changehee Choi <changhee.dev@gmail.com>
2020-12-03 18:24:16 +0900

    feat: Dashboard 페이지에 container 등록
    구현된 대시보드 메인 컨테이너를 대시보드 페이지에 등록함.
    대시보드 페이지에 메인 레이아웃 등록함.

--- client/src/components/common/layouts/MainLayout/styles.ts
+++ client/src/components/common/layouts/MainLayout/styles.ts
@@ -2,10 +2,10 @@ import styled from 'styled-components';
 
 const MainContainer = styled.div`
   width: 100%;
-  max-width: 800px;
+  max-width: 768px;
   height: 100%;
   margin: 0 auto;
-  padding-top: 51px;
+  padding: calc(51px + 1rem) 1rem 0;
 `;
 
 export default MainContainer;
