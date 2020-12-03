299280c26e4f7ba7a5617a2afbee2e31baec7d37
parkdit94 <dw1156@naver.com>
2020-12-03 22:02:53 +0900

    feat: 매트릭스 뷰 스타일 정의

---
+++ client/src/components/calendar/MatrixView/styles.ts
@@ -0,0 +1,31 @@
+import styled from 'styled-components';
+
+const Matrix = styled.div``;
+
+const Thead = styled.thead`
+  color: #9e9e9e;
+`;
+
+const Table = styled.table`
+  width: 100%;
+  height: 100%;
+  table-layout: fixed;
+  text-align: center;
+  border-spacing: none;
+  border-collapse: collapse;
+`;
+
+const Tbody = styled.tbody``;
+
+const Tr = styled.tr`
+  height: 70px;
+  font-size: large;
+  color: #9e9e9e;
+`;
+
+const Th = styled.th`
+  height: 30px;
+  vertical-align: middle;
+`;
+
+export { Table, Tbody, Tr, Thead, Matrix, Th };
