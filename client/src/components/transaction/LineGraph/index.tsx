35a5600b883b29766c29cd5ef1b2ed0cf344453b
박진용 <jinyoung1997@gmail.com>
2020-12-07 17:01:30 +0900

    feat : custom tooltip 추가

--- client/src/components/transaction/LineGraph/index.tsx
+++ client/src/components/transaction/LineGraph/index.tsx
@@ -1,10 +1,37 @@
 import React from 'react';
-import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend } from 'recharts';
+import {
+  LineChart,
+  Line,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+  TooltipProps,
+} from 'recharts';
 import dateUtil from '@libs/dateUtils';
 import { useSelector } from 'react-redux';
 import { RootState } from '@/modules';
-import StyledLineGraph from './styles';
-import { LineGraphProps, AggregateInfo } from './types';
+import * as S from './styles';
+import { LineGraphProps } from './types';
+
+const customTooltip = ({ active, payload, label }: TooltipProps) => {
+  return (
+    <>
+      {active && (
+        <S.CustomTooltipContainer>
+          <S.TooltipText>{`${label}일`}</S.TooltipText>
+          <S.TooltipText>
+            <S.TooltipColorBox color="#8884d8" /> {`수입 : ${payload ? payload[0].value : 0}원`}
+          </S.TooltipText>
+          <S.TooltipText>
+            <S.TooltipColorBox color="#82ca9d" /> {`지출 : ${payload ? payload[1].value : 0}원`}
+          </S.TooltipText>
+        </S.CustomTooltipContainer>
+      )}
+    </>
+  );
+};
 
 const LineGraph = ({ width, height, data }: LineGraphProps): JSX.Element => {
   const { datePicker } = useSelector((state: RootState) => state);
@@ -12,7 +39,7 @@ const LineGraph = ({ width, height, data }: LineGraphProps): JSX.Element => {
   const { maxTotal, graphData } = dateUtil.makeDataForLineGraph(data, year, month);
 
   return (
-    <StyledLineGraph>
+    <S.StyledLineGraph>
       <LineChart
         width={width}
         height={height}
@@ -22,12 +49,12 @@ const LineGraph = ({ width, height, data }: LineGraphProps): JSX.Element => {
         <XAxis dataKey="date" />
         <YAxis type="number" domain={[0, maxTotal]} />
         <CartesianGrid strokeDasharray="3 3" />
-        <Tooltip />
+        <Tooltip content={customTooltip} />
         <Legend />
         <Line type="monotone" dataKey="수입" stroke="#8884d8" activeDot={{ r: 8 }} />
         <Line type="monotone" dataKey="지출" stroke="#82ca9d" />
       </LineChart>
-    </StyledLineGraph>
+    </S.StyledLineGraph>
   );
 };
 
