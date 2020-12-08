27d8dbf646e4207138e3c6127f35a8c166f7dd2b
parkdit94 <dw1156@naver.com>
2020-12-08 11:34:26 +0900

    feat:드랍다운 버튼 이외 클릭 이벤트 추가

--- client/src/components/common/Dropdown/index.tsx
+++ client/src/components/common/Dropdown/index.tsx
@@ -4,20 +4,22 @@ import * as S from './styles';
 
 export default function Dropdown({ icon, isRight, children }: DropdownType): JSX.Element {
   const [isShow, setDisplay] = useState(false);
-  const classes = [];
-  if (isShow) {
-    classes.push('show');
-  }
-  if (isRight) {
-    classes.push('right');
-  }
+  const position = isRight ? 'right' : '';
 
+  const toggleDropdown = () => {
+    setDisplay(!isShow);
+  };
   return (
     <>
-      <S.Button onClick={() => setDisplay(!isShow)}>{icon}</S.Button>
-      <S.MenuItem className={classes.join(' ')}>
-        <ul>{children}</ul>
-      </S.MenuItem>
+      <S.IconDiv onClick={toggleDropdown}>{icon}</S.IconDiv>
+      {isShow && (
+        <>
+          <S.CloseDiv onClick={toggleDropdown} />
+          <S.MenuItem className={position} onClick={toggleDropdown}>
+            <ul>{children}</ul>
+          </S.MenuItem>
+        </>
+      )}
     </>
   );
 }
