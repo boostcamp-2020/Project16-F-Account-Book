7f760b1488bab85180b5c894bb377eaffb9a7a36
parkdit94 <dw1156@naver.com>
2020-12-09 00:22:51 +0900

    feat:dayPicker 타입 ㅊ ㅜ가

--- client/src/modules/datePicker/types.ts
+++ client/src/modules/datePicker/types.ts
@@ -1,7 +1,11 @@
-import { YearMonthModel } from '@/commons/types/date';
+import { YearMonthModel, SelectDayModel } from '@/commons/types/date';
 import { ActionType } from 'typesafe-actions';
 import * as actions from './actions';
 
 export type DatePickerAction = ActionType<typeof actions>;
 
 export type DatePickerState = YearMonthModel;
+
+export type DayPickerAction = ActionType<typeof actions>;
+
+export type DayPickerState = SelectDayModel;
