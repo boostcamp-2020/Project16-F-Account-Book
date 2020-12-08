502cca222cb67bd48eb3d28d19853ebd5f89c967
박진용 <jinyoung1997@gmail.com>
2020-12-08 23:39:20 +0900

    refactor : reducer 수정
    import를 하나로 묶어서 하고, delete failure 추가

--- client/src/modules/payment/reducer.ts
+++ client/src/modules/payment/reducer.ts
@@ -1,18 +1,6 @@
 import { PaymentModel } from '@/commons/types/payment';
 import { createReducer } from 'typesafe-actions';
-import {
-  DELETE_PAYMENT,
-  DELETE_PAYMENT_SUCCESS,
-  GET_PAYMENT,
-  GET_PAYMENT_FAILURE,
-  GET_PAYMENT_SUCCESS,
-  POST_PAYMENT,
-  POST_PAYMENT_FAILURE,
-  POST_PAYMENT_SUCCESS,
-  UPDATE_PAYMENT,
-  UPDATE_PAYMENT_FAILURE,
-  UPDATE_PAYMENT_SUCCESS,
-} from './actions';
+import * as paymentActions from './actions';
 import { PaymentAction, PaymentState } from './types';
 
 const initialState: PaymentState = {
@@ -22,44 +10,44 @@ const initialState: PaymentState = {
 };
 
 const paymentReducer = createReducer<PaymentState, PaymentAction>(initialState, {
-  [GET_PAYMENT]: (state) => ({
+  [paymentActions.GET_PAYMENT]: (state) => ({
     ...state,
     loading: true,
     error: null,
   }),
-  [GET_PAYMENT_SUCCESS]: (state, { payload }) => ({
+  [paymentActions.GET_PAYMENT_SUCCESS]: (state, { payload }) => ({
     ...state,
     loading: false,
     error: null,
     data: payload,
   }),
-  [GET_PAYMENT_FAILURE]: (state, { payload }) => ({
+  [paymentActions.GET_PAYMENT_FAILURE]: (state, { payload }) => ({
     ...state,
     loading: false,
     error: payload,
   }),
-  [POST_PAYMENT]: (state) => ({
+  [paymentActions.POST_PAYMENT]: (state) => ({
     ...state,
     loading: true,
     error: null,
   }),
-  [POST_PAYMENT_SUCCESS]: (state, { payload }) => ({
+  [paymentActions.POST_PAYMENT_SUCCESS]: (state, { payload }) => ({
     ...state,
     loading: false,
     error: null,
     data: state.data.concat(payload),
   }),
-  [POST_PAYMENT_FAILURE]: (state, { payload }) => ({
+  [paymentActions.POST_PAYMENT_FAILURE]: (state, { payload }) => ({
     ...state,
     loading: false,
     error: payload,
   }),
-  [UPDATE_PAYMENT]: (state) => ({
+  [paymentActions.UPDATE_PAYMENT]: (state) => ({
     ...state,
     loading: true,
     error: null,
   }),
-  [UPDATE_PAYMENT_SUCCESS]: (state, { payload }) => ({
+  [paymentActions.UPDATE_PAYMENT_SUCCESS]: (state, { payload }) => ({
     ...state,
     loading: false,
     data: state.data.map((payment) => {
@@ -67,20 +55,25 @@ const paymentReducer = createReducer<PaymentState, PaymentAction>(initialState, 
       return state.data;
     }) as PaymentModel[],
   }),
-  [UPDATE_PAYMENT_FAILURE]: (state, { payload }) => ({
+  [paymentActions.UPDATE_PAYMENT_FAILURE]: (state, { payload }) => ({
     ...state,
     loading: false,
     error: payload,
   }),
-  [DELETE_PAYMENT]: (state) => ({
+  [paymentActions.DELETE_PAYMENT]: (state) => ({
     ...state,
     loading: true,
     error: null,
   }),
-  [DELETE_PAYMENT_SUCCESS]: (state, { payload }) => ({
+  [paymentActions.DELETE_PAYMENT_SUCCESS]: (state, { payload }) => ({
     ...state,
     loading: false,
     data: state.data.filter((payment) => payment.pid !== payload.pid),
+  }),
+  [paymentActions.DELETE_PAYMENT_FAILURE]: (state, { payload }) => ({
+    ...state,
+    loading: false,
+    error: payload,
   }),
 });
 
