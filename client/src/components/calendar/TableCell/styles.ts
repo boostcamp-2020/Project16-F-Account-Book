dc289fd4cc24cf399430ea66423c4dc9c337ad4e
parkdit94 <dw1156@naver.com>
2020-12-09 02:20:41 +0900

    feat:테이블 셀 버튼 색 스타일 정의

--- client/src/components/calendar/TableCell/styles.ts
+++ client/src/components/calendar/TableCell/styles.ts
@@ -9,7 +9,18 @@ const TotalOut = styled.div`
   padding-left: 3px;
 `;
 const CellButton = styled.td`
+  position: relative;
   text-align: center;
 `;
 
-export { TotalIn, TotalOut, CellButton };
+const ClickCircle = styled.div`
+  position: absolute;
+  width: 60px;
+  height: 60px;
+  top: 0;
+  margin-left: 24px;
+  border-radius: 50%;
+  background-color: rgba(250, 185, 189, 0.6);
+`;
+
+export { TotalIn, TotalOut, CellButton, ClickCircle };
