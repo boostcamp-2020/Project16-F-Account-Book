0916955908ac38d51c32d4417f847101d41abb0b
parkdit94 <dw1156@naver.com>
2020-12-09 00:21:27 +0900

    feat:day를 관리하는  액션 추가

--- client/src/modules/datePicker/actions.ts
+++ client/src/modules/datePicker/actions.ts
@@ -1,4 +1,4 @@
-import { YearMonthModel } from '@/commons/types/date';
+import { YearMonthModel, SelectDayModel } from '@/commons/types/date';
 
 type ChangeDateActionType = {
   type: string;
@@ -9,5 +9,17 @@ export const CHANGE_DATE = 'date/CHANGE_DATE';
 
 export const changeDate = (date: YearMonthModel): ChangeDateActionType => ({
   type: CHANGE_DATE,
+  payload: date,
+});
+
+type ChangeDayActionType = {
+  type: string;
+  payload: SelectDayModel;
+};
+
+export const CHANGE_DAY = 'day/CHANGE_DAY';
+
+export const changeDay = (date: SelectDayModel): ChangeDayActionType => ({
+  type: CHANGE_DAY,
   payload: date,
 });
