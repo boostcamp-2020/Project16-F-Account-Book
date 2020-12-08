2873d7034c2d79e755b7274c9f56ff99ab6fa404
parkdit94 <dw1156@naver.com>
2020-12-09 02:16:10 +0900

    feat:캘린더뷰 버튼 클릭시 색 입히기 구현

--- client/src/components/calendar/CalendarView/index.tsx
+++ client/src/components/calendar/CalendarView/index.tsx
@@ -1,7 +1,4 @@
 import React from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { RootState } from '@/modules';
-import { changeDay } from '@modules/datePicker/actions';
 import MatrixView from '@/components/calendar/MatrixView';
 import TableCell from '@/components/calendar/TableCell';
 import getDayMatrix from '@libs/calendarUtils';
@@ -9,25 +6,12 @@ import { getWeekDays } from '@libs/nationalCalendarUtils';
 import { CalendarViewType } from './types';
 
 function CalendarView({ totalInOut, lang, year, month }: CalendarViewType): JSX.Element {
-  const { dayPicker } = useSelector((state: RootState) => state);
-  const dispatch = useDispatch();
-
-  const onClick = (e: any) => {
-    const value = e.currentTarget.innerText.split('\n')[0];
-    if (value === dayPicker.day) {
-      dispatch(changeDay({ day: 0 }));
-      return;
-    }
-    dispatch(changeDay({ day: value }));
-  };
   return (
     <>
       <MatrixView
         headers={getWeekDays(lang)}
         matrix={getDayMatrix(year, month)}
-        cell={(date, key) => (
-          <TableCell onClick={onClick} day={date} key={`table${key}`} totalInOut={totalInOut} />
-        )}
+        cell={(date, key) => <TableCell day={date} key={`table${key}`} totalInOut={totalInOut} />}
       />
     </>
   );
