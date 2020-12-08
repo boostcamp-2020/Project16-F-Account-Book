45a403f029b7d38080e1cec23446ceafcbf85de2
parkdit94 <dw1156@naver.com>
2020-12-09 00:34:12 +0900

    feat:TableCell style 추가

---
+++ client/src/components/calendar/TableCell/styles.ts
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+const TotalIn = styled.div`
+  text-align: center;
+`;
+
+const TotalOut = styled.div`
+  text-align: center;
+  padding-left: 3px;
+`;
+const CellButton = styled.td`
+  text-align: center;
+`;
+
+export { TotalIn, TotalOut, CellButton };
