964285ecbbcc8fb5bd1c9473c32e3707dd800e6a
Sangwoo <66261552+sangw3433@users.noreply.github.com>
2020-12-08 21:57:01 +0900

    feat : 고정 지출 상세 페이지 완료, 예정으로 분리
    고정 지출 상세 페이지 완료, 예정으로 분리해서 보이도록 구현

--- client/src/components/fixedExpenditure/Item/index.tsx
+++ client/src/components/fixedExpenditure/Item/index.tsx
@@ -3,14 +3,17 @@ import numberUtils from '@libs/numberUtils';
 import { FixedExpenditureItemPropType } from './types';
 import * as S from './styles';
 
-const FixedExpenditureItem = ({ fixedItem }: FixedExpenditureItemPropType): JSX.Element => {
+const FixedExpenditureItem = ({ fixedItem, isPaid }: FixedExpenditureItemPropType): JSX.Element => {
   return (
     <S.Box>
       <S.TitleBox>
         <S.Description>{fixedItem.description}</S.Description>
-        <S.Amount>약 {numberUtils.numberWithCommas(fixedItem.amount)} 원</S.Amount>
+        <S.Amount>
+          {isPaid ? '' : '약 '}
+          {numberUtils.numberWithCommas(fixedItem.amount)} 원
+        </S.Amount>
       </S.TitleBox>
-      <S.Date>{fixedItem.tradeAt.toString().substring(8)}일</S.Date>
+      <S.Date>{fixedItem.tradeAt.toString().substring(8, 10)}일</S.Date>
     </S.Box>
   );
 };
