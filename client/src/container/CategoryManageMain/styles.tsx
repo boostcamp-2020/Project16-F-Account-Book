9e73164ef7bee589cfb9b8b13c0b6816d037c736
박진용 <jinyoung1997@gmail.com>
2020-12-09 02:54:25 +0900

    feat : Category Manage Container 구현

---
+++ client/src/container/CategoryManageMain/styles.tsx
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+const CategoryListContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  margin-top: 1.5rem;
+`;
+
+export default CategoryListContainer;
