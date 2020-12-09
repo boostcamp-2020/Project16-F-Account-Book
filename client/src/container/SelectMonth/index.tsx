3bc94be715f60fa7fde8222ae086d817d7048b3e
parkdit94 <dw1156@naver.com>
2020-12-10 02:31:06 +0900

    feat:달력리스트 회원가입일 기준으로 변경

--- client/src/container/SelectMonth/index.tsx
+++ client/src/container/SelectMonth/index.tsx
@@ -11,8 +11,28 @@ import * as S from './styles';
 const MONTHSLIST = Array.from({ length: 12 }, (v, i) => 12 - i);
 
 export default function SelectMonth(): JSX.Element {
-  const { datePicker } = useSelector((state: RootState) => state);
+  const { datePicker, authorization } = useSelector((state: RootState) => state);
   const dispatch = useDispatch();
+  const [currentYear, currentMonth] = [new Date().getFullYear(), new Date().getMonth() + 1];
+  const [createYear, createMonth] = authorization.createAt?.toString().split('-').slice(0, 2);
+
+  const dataList = Array.from(
+    { length: currentYear - Number(createYear) + 1 },
+    (v, i) => currentYear - i,
+  ).map((v) => {
+    let Monthslenght = 0;
+    if (Number(createYear) === currentYear) Monthslenght = currentMonth - Number(createMonth) + 1;
+    if (v === currentYear && Number(createYear) !== currentYear) Monthslenght = currentMonth;
+    if (v === Number(createYear) && v !== currentYear) Monthslenght = 13 - Number(createMonth);
+    return [
+      v,
+      Array.from({ length: Monthslenght }, (value, i) => {
+        if (v === Number(createYear) && v !== currentYear) return 12 - i;
+        return currentMonth - i;
+      }),
+    ];
+  });
+
   const onChangeDate = (e: any) => {
     const { value } = e.target;
     dispatch(changeDate({ year: datePicker.year, month: MONTHSLIST[value] }));
@@ -39,13 +59,14 @@ export default function SelectMonth(): JSX.Element {
         <S.DropdownPosition>
           <Dropdown icon={dropdownButton} isRight={false}>
             <S.ScollDiv>
-              {MONTHSLIST.map((candidateDate: number, i: number) => (
-                <S.MonthList
-                  key={`candidateDate${i.toString()}`}
-                  value={i}
-                  onClick={onChangeDate}
-                >{`${datePicker.year}-${candidateDate}`}</S.MonthList>
-              ))}
+              {dataList.map((date) =>
+                [...date[1]].map((month, i) => (
+                  <S.MonthList
+                    key={`candidateDate${i.toString()}`}
+                    onClick={onChangeDate}
+                  >{`${date[0]}-${month}`}</S.MonthList>
+                )),
+              )}
             </S.ScollDiv>
           </Dropdown>
         </S.DropdownPosition>
