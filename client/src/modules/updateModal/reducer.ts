433aedd4e79f1d6e3d9ab755555d41f4b863a085
박진용 <jinyoung1997@gmail.com>
2020-12-10 00:30:06 +0900

    feat : updateModal 모듈 구현
    거래내역 수정을 위한 모달의 상태를 전역에서 관리하기 위해 모듈 구현

---
+++ client/src/modules/updateModal/reducer.ts
@@ -0,0 +1,22 @@
+import { TransactionModel } from '@/commons/types/transaction';
+import { createReducer } from 'typesafe-actions';
+import { TOGGLE_ON, TOGGLE_OFF } from './actions';
+import { UpdateModalAction, UpdateModalState } from './types';
+
+const initialState: UpdateModalState = {
+  toggle: false,
+  data: {} as TransactionModel,
+};
+
+const updateModalReducer = createReducer<UpdateModalState, UpdateModalAction>(initialState, {
+  [TOGGLE_ON]: (state, { payload }) => ({
+    toggle: true,
+    data: payload,
+  }),
+  [TOGGLE_OFF]: (state) => ({
+    ...state,
+    toggle: false,
+  }),
+});
+
+export default updateModalReducer;
---
+++ client/src/modules/updateModal/types.ts
@@ -0,0 +1,10 @@
+import { TransactionModel } from '@/commons/types/transaction';
+import { ActionType } from 'typesafe-actions';
+import * as actions from './actions';
+
+export type UpdateModalAction = ActionType<typeof actions>;
+
+export type UpdateModalState = {
+  toggle: boolean;
+  data: TransactionModel;
+};
