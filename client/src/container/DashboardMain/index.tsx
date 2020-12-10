4f945ec51b1922a6f01488c52bff86e0e86fbd6d
Changehee Choi <changhee.dev@gmail.com>
2020-12-10 17:16:35 +0900

    refactor: 대시보드 탭 통합
    대시보드의 나눠진 탭을 하나의 페이지로 합치고 컨텐츠 재배치 작업 진행.

--- client/src/container/DashboardMain/index.tsx
+++ client/src/container/DashboardMain/index.tsx
@@ -1,27 +1,116 @@
-import React, { useCallback, useEffect } from 'react';
+import React, { useCallback, useState, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { RootState } from '@modules/index';
 import { getMonthlyTransactionThunk } from '@/modules/transaction';
-import TabMenu from '@/components/common/TabMenu';
-import DashboardDetailsContainer from '@/container/DashboardDetails';
-import DashboardStaticsContainer from '@/container/DashboardStatics';
+import { Link } from 'react-router-dom';
+import SelectMonth from '@/container/SelectMonth';
+import TransactionListItem from '@/components/transaction/ListItem';
+import AmountText from '@/components/transaction/AmountText';
+import FixedExpenditure from '@container/FixedExpenditure';
+import aggregateAPI from '@/libs/api/aggregate';
+import NumberUtils from '@/libs/numberUtils';
+import * as S from './styles';
+
+const RECENT_TRANSACTION_LIMIT = 3;
 
 const DashboardContainer = (): JSX.Element => {
-  const { datePicker } = useSelector((state: RootState) => state);
+  const { transaction, datePicker } = useSelector((state: RootState) => state);
   const dispatch = useDispatch();
   const getMonthlyTransactions = useCallback(() => {
     dispatch(getMonthlyTransactionThunk(datePicker));
-  }, [dispatch]);
+  }, [dispatch, datePicker]);
+  const [overspendingIndexState, setOverspendingIndexState] = useState({
+    overspendingIndex: 0,
+    averageIncome: 0,
+    expenditureThisMonth: 0,
+  });
+  const [mostSpendingCategoryState, setMostSpendingCategoryState] = useState({
+    name: '',
+    aggregate: '',
+  });
 
   useEffect(() => {
     getMonthlyTransactions();
+    Promise.all([
+      aggregateAPI.getMostSpendingCategory(datePicker.year, datePicker.month),
+      aggregateAPI.getOverspendingIndex(datePicker.year, datePicker.month),
+    ]).then(([mostSpendingCategory, overspendingIndex]) => {
+      setMostSpendingCategoryState(mostSpendingCategory);
+      setOverspendingIndexState(overspendingIndex);
+    });
   }, [datePicker]);
 
-  const tabMenu = [
-    { tabName: '내역', children: <DashboardDetailsContainer /> },
-    { tabName: '통계', children: <DashboardStaticsContainer /> },
-  ];
-  return <TabMenu list={tabMenu} />;
+  const getSpendingStatus = useCallback((overspendingIndex: number): string => {
+    // 1 이상 : 재정적으로 매우 위험한 상태
+    // 0.7~1 미만 : 과소비 상태
+    // 0.5~0.7 미만 : 수입과 지출의 비중이 적정한 상태
+    // 0.5 미만 : 근검절약을 실천하는 훌륭한 상태
+    if (overspendingIndex >= 1) {
+      return '매우 위험한 상태입니다';
+    }
+    if (overspendingIndex >= 0.7) {
+      return '좋지 않네요 😢';
+    }
+    if (overspendingIndex >= 0.5) {
+      return '나쁘지 않아요 🙂';
+    }
+    return '훌륭하네요 😍';
+  }, []);
+
+  return (
+    <>
+      <S.SelectorBox>
+        <SelectMonth />
+      </S.SelectorBox>
+      <S.Box>
+        <S.BoxHeader>
+          <S.BoxTitle>{datePicker.month}월 소비/수입</S.BoxTitle>
+          <S.SpendingStatusDescription>
+            이번달 소비 습관이 {getSpendingStatus(overspendingIndexState.overspendingIndex)}
+          </S.SpendingStatusDescription>
+        </S.BoxHeader>
+        <S.BoxRow>
+          <AmountText isIncome={false} size="lg" amount={transaction.totalOut} />
+        </S.BoxRow>
+        <S.BoxRow>
+          <AmountText isIncome size="lg" amount={transaction.totalIn} />
+        </S.BoxRow>
+      </S.Box>
+      <S.Box>
+        <S.BoxHeader>
+          <S.BoxTitle>최근 내역</S.BoxTitle>
+          <Link to="/calendar">자세히 보기</Link>
+        </S.BoxHeader>
+        {transaction.transactions.slice(0, RECENT_TRANSACTION_LIMIT).map((mockTransaction) => (
+          <S.RecentTransactionBoxItem key={`transaction${mockTransaction.tid}`}>
+            <TransactionListItem transaction={mockTransaction} />
+          </S.RecentTransactionBoxItem>
+        ))}
+      </S.Box>
+      <FixedExpenditure />
+      <S.Box>
+        <S.BoxHeader>
+          <S.BoxTitle>카테고리 통계</S.BoxTitle>
+          <Link to="/calendar">자세히 보기</Link>
+        </S.BoxHeader>
+        <S.BoxRow>{mostSpendingCategoryState.name}에 가장 많은 돈을 쓰셨어요</S.BoxRow>
+        <S.BoxRow>
+          사용한 금액 : {NumberUtils.numberWithCommas(Number(mostSpendingCategoryState.aggregate))}
+          원
+        </S.BoxRow>
+      </S.Box>
+      <S.Box>
+        <S.BoxHeader>
+          <S.BoxTitle>기간별 통계</S.BoxTitle>
+          <Link to="/calendar">자세히 보기</Link>
+        </S.BoxHeader>
+        <S.BoxRow>{transaction.mostOutDateDetail.date}일에 가장 많은 돈을 쓰셨어요</S.BoxRow>
+        <S.BoxRow>
+          사용한 금액 : {NumberUtils.numberWithCommas(transaction.mostOutDateDetail.amount)}원
+        </S.BoxRow>
+      </S.Box>
+    </>
+  );
 };
 
 export default DashboardContainer;
