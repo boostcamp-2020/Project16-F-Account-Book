53acfb7be5239cd6ad2dd492267f84c7299a9d17
Changehee Choi <changhee.dev@gmail.com>
2020-12-11 02:55:56 +0900

    refactor: 금액표시 Text 지출일때의 색상 변경

--- client/src/components/transaction/AmountText/styles.ts
+++ client/src/components/transaction/AmountText/styles.ts
@@ -2,13 +2,13 @@ import styled from 'styled-components';
 import { StyledAmountTextProps } from './types';
 
 const StyledAmountText = styled.span<StyledAmountTextProps>`
-  color: ${(props) => (props.isIncome ? '#4B88C4' : '#0F0F0F')};
+  color: ${(props) => (props.isIncome ? '#4B88C4' : '#c2255c')};
   font-size: ${(props) => {
     if (props.size === 'lg') {
       return '1.5rem';
     }
     if (props.size === 'sm') {
-      return '0.8rem';
+      return '0.9rem';
     }
     if (props.size === 'xs') {
       return '0.5rem';
