4e2bda650e305330dfe76b802e7e36d4b3750d92
Sangwoo <66261552+sangw3433@users.noreply.github.com>
2020-12-11 11:01:21 +0900

    feat : 모달 버튼 추가
    모달 버튼 추가

--- client/src/views/CalendarPage/index.tsx
+++ client/src/views/CalendarPage/index.tsx
@@ -1,12 +1,18 @@
-import React from 'react';
+import React, { useCallback, useState } from 'react';
 import MainLayout from '@/components/common/layouts/MainLayout';
 import Calendar from '@container/Calendar';
 import TransactionUpdateModal from '@/container/TransactionUpdateModal';
+import TransactionModal from '@container/TransactionModal';
+import ModalToggleButton from '@components/common/buttons/ModalToggleButton';
 
 const CalendarPage = (): JSX.Element => {
+  const [showModal, setShowModal] = useState(false);
+  const toggleModal = useCallback(() => setShowModal(!showModal), [showModal]);
   return (
     <MainLayout>
       <Calendar />
+      <ModalToggleButton setToggle={toggleModal} />
+      <TransactionModal show={showModal} toggleModal={toggleModal} />
       <TransactionUpdateModal />
     </MainLayout>
   );
