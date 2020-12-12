7cee6013c22bae91476bf1c55d480f5e22f3bc63
Sangwoo <66261552+sangw3433@users.noreply.github.com>
2020-12-13 00:48:40 +0900

    feat : 라인그래프 페이지 데이터 비어있을 경우 처리
    데이터가 없을 경우 내역이 없다고 표시

--- client/src/container/LineGraphContainer/index.tsx
+++ client/src/container/LineGraphContainer/index.tsx
@@ -2,20 +2,25 @@ import LineGraph from '@/components/transaction/LineGraph';
 import { RootState } from '@/modules';
 import React from 'react';
 import { useSelector } from 'react-redux';
+import EmptyStateComponent from '@/components/transaction/EmptyState';
 import SelectMonth from '../SelectMonth';
 import * as S from './styles';
 
-const LineGraphContainer = () => {
+const LineGraphContainer = (): JSX.Element => {
   const { transaction } = useSelector((state: RootState) => state);
   return (
     <>
       {transaction && (
         <>
           <SelectMonth />
-          <S.StyledLineGraphContainer>
-            <S.LineGraphText>기간별 통계</S.LineGraphText>
-            <LineGraph data={transaction.aggregationByDate} />
-          </S.StyledLineGraphContainer>
+          {transaction.aggregationByDate.length === 0 ? (
+            <EmptyStateComponent />
+          ) : (
+            <S.StyledLineGraphContainer>
+              <S.LineGraphText>기간별 통계</S.LineGraphText>
+              <LineGraph data={transaction.aggregationByDate} />
+            </S.StyledLineGraphContainer>
+          )}
         </>
       )}
     </>
