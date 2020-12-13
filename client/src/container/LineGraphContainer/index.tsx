f946251a99b2e93db3a73b824792f338cd59ab67
박진용 <jinyoung1997@gmail.com>
2020-12-14 02:27:44 +0900

    feat : 고정지출 선형 그래프 페이지 loading 추가

--- client/src/container/LineGraphContainer/index.tsx
+++ client/src/container/LineGraphContainer/index.tsx
@@ -1,3 +1,4 @@
+import LoadingSpinner from '@/components/common/LoadingSpinner';
 import LineGraph from '@/components/transaction/LineGraph';
 import { RootState } from '@/modules';
 import React from 'react';
@@ -9,7 +10,9 @@ const LineGraphContainer = () => {
   const { transaction } = useSelector((state: RootState) => state);
   return (
     <>
-      {transaction && (
+      {transaction.loading ? (
+        <LoadingSpinner />
+      ) : (
         <>
           <SelectMonth />
           <S.StyledLineGraphContainer>
