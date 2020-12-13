12f94d48ce2660fe66ac31425e7ccba0956c4c6a
박진용 <jinyoung1997@gmail.com>
2020-12-13 23:04:49 +0900

    feat : 대시보드 페이지 loading component 추가

---
+++ client/src/components/common/LoadingSpinner/styles.ts
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+const StyledLoadingSpinner = styled.div`
+  display: flex;
+  height: 100%;
+  width: 100%;
+  justify-content: center;
+  align-items: center;
+  .img {
+    height: 30%;
+  }
+`;
+
+export default StyledLoadingSpinner;
