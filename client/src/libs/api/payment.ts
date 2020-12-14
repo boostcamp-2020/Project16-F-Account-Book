872562ce5cc5a7be8d5a11cf2bd6377a0092ac34
Changehee Choi <changhee.dev@gmail.com>
2020-12-14 23:37:19 +0900

    feat: 결제수단 API 호출 모듈 캐싱 적용

--- client/src/libs/api/payment.ts
+++ client/src/libs/api/payment.ts
@@ -2,15 +2,22 @@ import PaymentRequestDTO from '@/commons/dto/payment-request';
 import axios from '@/libs/axios';
 import endpoints from '@/libs/endpoints';
 import { PaymentModel } from '@commons/types/payment';
+import cache from '@/libs/cache/paymentCache';
 
 const paymentAPI = {
   getPayment: async (): Promise<PaymentModel[]> => {
+    const cachedData = cache.get();
+    if (cachedData.length !== 0) {
+      return cachedData;
+    }
     const payments = await axios.get<PaymentModel[]>(endpoints.PAYMENT_METHOD_API);
+    cache.set(payments);
     return payments;
   },
 
   postPayment: async (data: PaymentRequestDTO): Promise<PaymentModel> => {
     const payment = await axios.post<PaymentModel>(endpoints.PAYMENT_METHOD_API, { ...data });
+    cache.clear();
     return payment;
   },
 
@@ -18,11 +25,13 @@ const paymentAPI = {
     const payment = await axios.patch<PaymentModel>(`${endpoints.PAYMENT_METHOD_API}/${data.pid}`, {
       ...data,
     });
+    cache.clear();
     return payment;
   },
 
   deletePayment: async (pid: number): Promise<PaymentModel> => {
     const payment = await axios.delete<PaymentModel>(`${endpoints.PAYMENT_METHOD_API}/${pid}`);
+    cache.clear();
     return payment;
   },
 };
