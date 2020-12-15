d29b8f011038d45a7fe8d1241692f02816e83170
parkdit94 <dw1156@naver.com>
2020-12-15 22:45:20 +0900

    fix:파서 유효성검사 버그수정

--- client/src/container/TransactionModal/index.tsx
+++ client/src/container/TransactionModal/index.tsx
@@ -49,14 +49,14 @@ const TransactionModal = ({ show, toggleModal }: TransactionModalProps): JSX.Ele
     if (checkValidation(e.target.name, e.target.value)) {
       infoDispatch({ ...newTransaction, [e.target.name]: e.target.value });
       validation.add(e.target.name);
-      setValidation(new Set([...validation]));
+      setValidation(validation);
     } else {
       validation.delete(e.target.name);
-      setValidation(new Set([...validation]));
+      setValidation(validation);
     }
     if ((e.target.name === 'description' || e.target.name === 'amount') && e.target.value === '') {
       validation.delete(e.target.name);
-      setValidation(new Set([...validation]));
+      setValidation(validation);
     }
   };
 
@@ -80,7 +80,7 @@ const TransactionModal = ({ show, toggleModal }: TransactionModalProps): JSX.Ele
         checkEmpty(['', `${parsedText.amount}`, clipText, 'false']).map((addList) =>
           validation.add(addList),
         );
-        setValidation(new Set([...validation]));
+        setValidation(validation);
         setIsIncome(false);
       } else {
         const { year: thisYear } = DateUtils.parseDate(new Date());
@@ -99,7 +99,7 @@ const TransactionModal = ({ show, toggleModal }: TransactionModalProps): JSX.Ele
           clipText,
           `${parsedText.isDeposit}`,
         ]).map((addList) => validation.add(addList));
-        setValidation(new Set([...validation]));
+        setValidation(validation);
       }
     });
   }, []);
