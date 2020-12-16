eb8577acb10de5a2770ea095fe6b6170c4f8a28f
parkdit94 <dw1156@naver.com>
2020-12-16 16:36:51 +0900

    refactor:amountText 로직 변경

--- client/src/components/transaction/AmountText/styles.ts
+++ client/src/components/transaction/AmountText/styles.ts
@@ -3,21 +3,7 @@ import { StyledAmountTextProps } from './types';
 
 const StyledAmountText = styled.span<StyledAmountTextProps>`
   color: ${(props) => (props.isIncome ? '#4B88C4' : '#c2255c')};
-  font-size: ${(props) => {
-    if (props.size === 'lg') {
-      return '1.5rem';
-    }
-    if (props.size === 'sm') {
-      return '0.9rem';
-    }
-    if (props.size === 'xs') {
-      return '0.5rem';
-    }
-    if (props.size === '') {
-      return '1rem';
-    }
-    return `${props.size}`;
-  }};
+  font-size: ${(props) => props.size};
   font-weight: 600;
 `;
 
