e52b378b54ec2d56c84602662e6f4f9a78be4a39
박진용 <jinyoung1997@gmail.com>
2020-12-19 21:39:39 +0900

    feat : demo 로그인 버튼 추가

--- client/src/container/LoginButton/styles.tsx
+++ client/src/container/LoginButton/styles.tsx
@@ -1,12 +1,19 @@
 import styled from 'styled-components';
 
-const StyledButtonBox = styled.div`
+export const StyledButtonBox = styled.div`
   width: 80%;
   display: flex;
   flex-direction: column;
   margin-left: auto;
   margin-right: auto;
-  margin-top: 10vh;
+  margin-top: 9vh;
 `;
 
-export default StyledButtonBox;
+export const OrSeparator = styled.div`
+  border-top: 1px solid #eee;
+  padding: 10px 0;
+  flex: 0 auto;
+  margin-top: 20px;
+
+  font-size: 1em;
+`;
