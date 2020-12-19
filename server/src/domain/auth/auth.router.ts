da60587c130b73c2f4245a38f99ed2ee66bf9c6a
박진용 <jinyoung1997@gmail.com>
2020-12-19 21:38:08 +0900

    feat : demo 로그인 추가
    서버에서 생성한 랜덤 데이터 접속가능하게 설정

--- server/src/domain/auth/auth.router.ts
+++ server/src/domain/auth/auth.router.ts
@@ -8,6 +8,7 @@ import OAuthClient from '@/lib/oauth-client';
 import BadRequest from '@/common/error/bad-request';
 import JwtUtils from '@/lib/jwt-utils';
 import { JwtConfig } from '@/config';
+import ServerError from '@/common/error/server-error';
 
 class AuthRouter extends Router {
   private userService;
@@ -25,6 +26,25 @@ class AuthRouter extends Router {
     this.post('/logout', jwtAuthorize, (ctx: Context) => {
       ctx.cookies.set('jwt');
       ctx.status = 204;
+    });
+
+    this.get('/test-login', async (ctx: Context) => {
+      const TEST_UID_START = Number(process.env.TEST_UID_START as string);
+      const TEST_UID_END = Number(process.env.TEST_UID_END as string);
+
+      if (!TEST_UID_START || !TEST_UID_END) {
+        throw new ServerError('Test uid undefined');
+      }
+      const randomUserId = Math.floor(
+        Math.random() * (TEST_UID_END - TEST_UID_START) + TEST_UID_START,
+      );
+
+      const user = await this.userService.getUserById(randomUserId);
+      const jwtUtils = new JwtUtils(JwtConfig);
+      const jwtToken = jwtUtils.generateToken(user);
+      jwtUtils.setCookie(ctx, jwtToken);
+      const clientUri = process.env.CLIENT_URI as string;
+      ctx.redirect(clientUri);
     });
 
     this.get('/:provider', (ctx: Context) => {
